83999c30b75f6edbc244f40d3f25024dfa3bd0a9
“Soumik” <jasoumik@gmail.com>
2025-03-15 22:43:53 +0600

    fixed public routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  root: 'src', // Set the root to the 'src' folder
+  // root: 'src', // Set the root to the 'src' folder
   base: '/', // Base URL for the project
   build: {
     outDir: '../dist', // Output directory for the build
