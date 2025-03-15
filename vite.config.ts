87f3c80ef47b50f58f5c6c71a27530a7b0bd39f8
“Soumik” <jasoumik@gmail.com>
2025-03-15 14:03:54 +0600

    Fix script path in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  root: 'src',
-  base: '/',
+  root: 'src', // Set the root to the 'src' folder
+  base: '/', // Base URL for the project
   build: {
-    outDir: '../dist',
-    emptyOutDir: true,
+    outDir: '../dist', // Output directory for the build
+    emptyOutDir: true, // Ensure the output directory is emptied before building
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'), // Add alias for src folder
     },
   },
   plugins: [react()],
