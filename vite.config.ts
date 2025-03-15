8559f0c248571912373eb3d7778ed914c2c6ce57
“Soumik” <jasoumik@gmail.com>
2025-03-15 23:30:50 +0600

    fixed module script

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,9 @@ export default defineConfig({
   // root: 'src', // Set the root to the 'src' folder
   base: '/', // Base URL for the project
   build: {
-    outDir: '../dist', // Output directory for the build
-    emptyOutDir: true, // Ensure the output directory is emptied before building
+    outDir: 'dist', // Ensures everything is compiled into dist/
+    emptyOutDir: true,
+    manifest: true, // Generates a manifest.json file
   },
   resolve: {
     alias: {
