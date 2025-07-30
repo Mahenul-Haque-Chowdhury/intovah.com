78a366717bfa6b8e6445504354ae29ce005ad33d
“Soumik” <jasoumik@gmail.com>
2025-07-31 01:46:07 +0600

    used meta instead of script

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -19,11 +19,7 @@ function Layout({
       )}
     >
       <Helmet>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8285428347390731"
-          crossOrigin="anonymous"
-        ></script>
+        <meta name="google-adsense-account" content="ca-pub-8285428347390731" />
       </Helmet>
 
 
